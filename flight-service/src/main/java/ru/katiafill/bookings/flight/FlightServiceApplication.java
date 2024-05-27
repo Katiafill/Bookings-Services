cde1d348dbe293032dc17743795fe51053b771ce
katiafill <katiafill@mail.ru>
2024-05-27 10:15:55 +0700

    Создан новый сервис flight-service

---
+++ flight-service/src/main/java/ru/katiafill/bookings/flight/FlightServiceApplication.java
@@ -0,0 +1,15 @@
+package ru.katiafill.bookings.flight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+@RefreshScope
+public class FlightServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightServiceApplication.class, args);
+    }
+
+}
